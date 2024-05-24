a233dd13f351da22ee79a8698fe697d68a12dce0
enjayyy <naoji@st.ug.edu.gh>
2024-05-24 16:06:38 +0000

    Added README

--- my-app/App.js
+++ my-app/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>My name is <Text style={{fontWeight: "bold"}}>Nana Oji</Text></Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -12,6 +13,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    fontSize: 24,
     flex: 1,
     backgroundColor: '#FFC300',
     alignItems: 'center',
